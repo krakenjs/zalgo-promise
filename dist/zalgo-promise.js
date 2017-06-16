26e4341ba06e348dbdc0301c23a95d22f96f0958
Daniel Brain <dbrain@paypal.com>
2017-06-16 13:43:15 -0700

    Dist

--- dist/zalgo-promise.js
+++ dist/zalgo-promise.js
@@ -81,7 +81,11 @@
                 value: !0
             });
             exports.ZalgoPromise = void 0;
-            var _createClass = function() {
+            var _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function(obj) {
+                return typeof obj;
+            } : function(obj) {
+                return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+            }, _createClass = function() {
                 function defineProperties(target, props) {
                     for (var i = 0; i < props.length; i++) {
                         var descriptor = props[i];
@@ -165,30 +169,38 @@
                     value: function() {
                         var _this3 = this, resolved = this.resolved, rejected = this.rejected, handlers = this.handlers;
                         if (resolved || rejected) for (;handlers.length; ) {
-                            (function() {
-                                var _handlers$shift = handlers.shift(), onSuccess = _handlers$shift.onSuccess, onError = _handlers$shift.onError, promise = _handlers$shift.promise, isError = !1, result = void 0, error = void 0;
+                            var _ret = function() {
+                                var _handlers$shift = handlers.shift(), onSuccess = _handlers$shift.onSuccess, onError = _handlers$shift.onError, promise = _handlers$shift.promise, result = void 0;
                                 if (resolved) try {
                                     result = onSuccess ? onSuccess(_this3.value) : _this3.value;
                                 } catch (err) {
-                                    isError = !0;
-                                    error = err;
-                                } else if (rejected) if (onError) try {
-                                    result = onError(_this3.error);
-                                } catch (err) {
-                                    isError = !0;
-                                    error = err;
-                                } else {
-                                    isError = !0;
-                                    error = _this3.error;
+                                    return {
+                                        v: promise.reject(err)
+                                    };
+                                } else if (rejected) {
+                                    if (!onError) return {
+                                        v: promise.reject(_this3.error)
+                                    };
+                                    try {
+                                        result = onError(_this3.error);
+                                    } catch (err) {
+                                        return {
+                                            v: promise.reject(err)
+                                        };
+                                    }
                                 }
                                 if (result === _this3) throw new Error("Can not return a promise from the the then handler of the same promise");
-                                if (!promise) return "continue";
-                                isError ? promise.reject(error) : (0, _utils.isPromise)(result) ? result.then(function(res) {
-                                    promise.resolve(res);
-                                }, function(err) {
-                                    promise.reject(err);
-                                }) : promise.resolve(result);
-                            })();
+                                return (0, _utils.isPromise)(result) ? {
+                                    v: result.then(function(res) {
+                                        promise.resolve(res);
+                                    }, function(err) {
+                                        promise.reject(err);
+                                    })
+                                } : {
+                                    v: promise.resolve(result)
+                                };
+                            }();
+                            if ("object" === (void 0 === _ret ? "undefined" : _typeof(_ret))) return _ret.v;
                         }
                     }
                 }, {
