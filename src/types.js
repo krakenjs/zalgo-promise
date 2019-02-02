53bd729dc9b90e40a8fd81a3fb026b5d1ec12d46
Daniel Brain <daniel@brainblocks.io>
2019-02-01 16:22:04 -0800

    Add export to types

--- src/types.js
+++ src/types.js
@@ -2,3 +2,6 @@
 
 export type ZalgoPromiseConstructorType =
     (resolve : (mixed) => void, reject : (mixed) => void) => void;
+
+// export something to force webpack to see this as an ES module
+export const TYPES = true;
