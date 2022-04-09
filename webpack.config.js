a0978c0b47a93ea85f8df46f6b29a6ba54596d17
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-04-09 16:10:56 -0500

    chore: move devDependencies to @krakenjs scope

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 /* @flow */
 /* eslint import/no-nodejs-modules: off, import/no-default-export: off */
 
-import type { WebpackConfig } from 'grumbler-scripts/config/types';
-import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
+import type { WebpackConfig } from '@krakenjs/grumbler-scripts/config/types';
+import { getWebpackConfig } from '@krakenjs/grumbler-scripts/config/webpack.config';
 
 export const FILE_NAME = 'zalgo-promise';
 export const MODULE_NAME = 'ZalgoPromise';
